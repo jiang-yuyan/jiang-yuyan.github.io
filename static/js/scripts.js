cf7802340d8dde10398cc1ed878fbcded910bff9
jiang-yuyan <44131309+jiang-yuyan@users.noreply.github.com>
2025-01-18 19:03:29 +0800

    scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards', 'projects']
+const section_names = ['home','research interests', 'academic identities', 'publications', 'awards', 'projects']
 
 
 window.addEventListener('DOMContentLoaded', event => {
